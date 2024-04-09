76f42b20284d9f1b13b346dc23402f7166ef5bca
fernanda-escobar2 <chinafernanda203@gmail.com>
2024-04-09 15:18:32 -0600

    Edit

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,22 +1,30 @@
 <x-layout>
-    <h1 class="text-3xl font-bold text-center">Log In</h1>
-    <form class="text-center" action="/login" method="POST" >
-        @csrf
+    <div class="grid grid-cols-2 gap-4 place content-center py-8">
+        <img class= "" src="https://i.pinimg.com/564x/5a/55/92/5a55924b45893cd7be1671df8a62a5d5.jpg" alt="">
 
-        <div class="space-y-10">
-            <x-input title="Email" type="text" nombre="email"/>
-            @error('email')
-                <p>{{ $message }}</p>
-            @enderror
+        <div class="w-80 p-8 bg-red-200 rounded-lg shadow-md">
+
+            <h1 class="text-3xl font-bold text-center">Log In</h1>
+            <form class="text-center" action="/login" method="POST">
+                @csrf
+
+                <div class="space-y-10">
+                    <x-input title="Email" type="text" nombre="email" />
+                    @error('email')
+                        <p>{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="space-y-8 ">
+                    <x-input title="Password" type="password" nombre="password" />
+                    @error('password')
+                        <p>{{ $message }} </p>
+                    @enderror
+                </div>
+
+                <button type="submit" class="text-xl border mt-8 p-6 font-serif">Log in</button>
+            </form>
         </div>
 
-        <div class="space-y-8">
-            <x-input title="Password" type="password" nombre="password"/>
-            @error('password')
-                <p>{{ $message }} </p>
-            @enderror
-        </div>
-
-        <button type="submit" class="text-xl border mt-5 p-4 font-serif">Log in</button>
-    </form>
+    </div>
 </x-layout>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,42 +1,47 @@
+<x-layout/>
+<center>
+<h1 class="place-content-center text-blue-700 mb-6 text-2xl font-bold pb-8 "> Register </h1>
+</center>
 <div style= "display: flex; justify-content: center; aling-items: center; height: 100vh;">
-    <form action="/Register" method="post" style="background-color #ecfdf5; padding: 15px; border-radius: 10px; width: 400px;">
+    <form action="/register" method="POST" style="background-color: #a2e7fc; padding: 15px; border-radius: 10px; width: 400px;">
         @csrf
         <div style= "margin-bottom: 15px; text-aling: center;">
-            <label for= "name" style="color: #fffbeb; display: block; margin-bottom: 6px; font-size: 20px;" >Name:</label>
-            <input type="text" name="name" style="width: 100%; padding: 9px; margin-botoom: 8px; borde: 1px solid #ccc; border-radius: 5px; box-sizing: border-box; font-size: 20px;">
+            <label for= "name" style="color: hsl(303, 88%, 68%); display: block; margin-bottom: 6px; font-size: 20px;" >Name:</label>
+            <input type="text" name="name" class="border border-[#cf0202]" style="width: 100%; padding: 9px; margin-botoom: 8px; rounded-md; box-sizing: border-box; font-size: 20px;">
             @error('name')
-                <p style="color: #f0fdf4; margin-top: -8px; margin-bottom: 8px; font-size: 20px;"> {{$message}}</p>
+                <p style="color: #81ddd5; margin-top: -8px; margin-bottom: 8px; font-size: 20px;"> {{$message}}</p>
             @enderror
         </div>
 
          <div style= "margin-bottom: 15px; text-aling: center;">
-            <label for="email"style="color: #fffbeb; display: block; margin-bottom: 6px; font-size: 20px;" >Email:</label>
-            <input type="text" name="email"  style="width: 100%; padding: 9px; margin-botoom: 8px; border: 1px solid #ccc; border-radius: 5px; box-sizing: border-box; font-size: 20px;">
+            <label for="email"style="color: hsl(303, 88%, 68%); display: block; margin-bottom: 6px; font-size: 20px;" >Email:</label>
+            <input type="text" name="email" class="border border-[#cf0202]" style="width: 100%; padding: 9px; margin-botoom: 8px; rounded-md; box-sizing: border-box; font-size: 20px;">
             @error('email')
-                <p style="color: #f0fdf4; margin-top: -8px; margin-bottom: 8px; font-size: 20px;"> {{$message}} </p>
+                <p style="color: #17d321; margin-top: -8px; margin-bottom: 8px; font-size: 20px;"> {{$message}} </p>
             @enderror
         </div>
 
          <div style= "margin-bottom: 15px; text-aling: center;" >
-            <label for="password" style="color: #fffbeb; display: block; margin-bottom: 6px; font-size: 20px;">Password:</label>
-            <input type="password" name="password" style="width: 100%; padding: 9px; margin-botoom: 8px; borde: 1px solid #ccc; border-radius: 5px; box-sizing: border-box; font-size: 20px;">
+            <label for="password" style="color: hsl(303, 88%, 68%); display: block; margin-bottom: 6px; font-size: 20px;">Password:</label>
+            <input type="password" name="password" class="border border-[#cf0202]" style="width: 100%; padding: 9px; margin-botoom: 8px; rounded-md; box-sizing: border-box; font-size: 20px;">
             @error('password')
-                <p style="color: #f0fdf4; margin-top: -8px; margin-botton 8px; font-size: 20px;"> {{$message}} </p>
+                <p style="color: #09c441; margin-top: -8px; margin-botton 8px; font-size: 20px;"> {{$message}} </p>
             @enderror
         </div>
 
          <div style= "margin-bottom: 15px; text-aling: center;" >
-            <label for="birthday" style="color: #fffbeb; display: block; margin-bottom: 6px; font-size: 20px;">Birthday:</label>
-            <input type="date" name="birthday" style="width: 100%; padding: 9px; margin-botoom: 8px; borde: 1px solid #ccc; border-radius: 5px; box-sizing: border-box; font-size: 20px;" >
+            <label for="birthday" style="color: hsl(303, 88%, 68%); display: block; margin-bottom: 6px; font-size: 20px;">Birthday:</label>
+            <input type="date" name="birthday" class= "border border-[#cf0202]" style="width: 100%; padding: 9px; margin-botoom: 8px; rounded-md; box-sizing: border-box; font-size: 20px;" >
             @error('birthday')
-                <p style="color: #f0fdf4; margin-top: -8px; margin-botton 8px; font-size: 20px;"> {{$message}} </p>
+                <p style="color: #19cf50; margin-top: -8px; margin-botton 8px; font-size: 20px;"> {{$message}} </p>
             @enderror
         </div>
 
          <div style= "text-align: center;">
-        <button type="submit" class="background-color: #fffbeb; color: red; font-size: 18px; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer; ">Register</button>
+        <button type="submit" class="background-color:#eb0505;  font-size: 18px; padding: 10px 20px; border border-xl none; rounded-md; cursor: pointer; ">Register</button>
     </form>
 </div>
+<x-layout/>
 
 
 
